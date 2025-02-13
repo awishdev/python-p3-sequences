61932d7c0540be2d8a18141a6c096bb41a9234bb
Arthur Ish <arthurishdev@gmail.com>
2025-02-13 13:48:25 -0800

    The golden ratio

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    a = 0
+    b = 1
+    for fib in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    print(fib_sequence)
